b927340a6c6dea3a62ba128e6f61abf659a03ac1
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2021-12-06 12:08:22 +0900

    Add files via upload

--- dowtube.py
+++ dowtube.py
@@ -1,4 +1,5 @@
 import os
+import validators
 from pytube import YouTube
 from pytube.cli import on_progress
 from termcolor import colored
@@ -7,17 +8,33 @@ from pyfiglet import Figlet
 
 os.system('cls')
 
+
+def inputlink():
+    link = input("Insira o link: ")
+    valink = validators.url(link)
+   
+    if not valink:
+        del link
+        inputlink()
+        return -1
+    
+  
+    yt = YouTube(link, on_progress_callback = on_progress)
+
+    print("Titulo = ", yt.title)
+    print("Baixando...")
+
+    ys = yt.streams.get_highest_resolution()
+
+    ys.download()
+
 f = Figlet(font='standard')
 print(colored(f.renderText('DowTube'), 'red'))
 
-link = input("Insira o link: ")
+inputlink()
 
-yt = YouTube(link, on_progress_callback = on_progress)
 
-print("Titulo = ", yt.title)
-print("Baixando...")
 
-ys = yt.streams.get_highest_resolution()
 
-ys.download()
+    
 
