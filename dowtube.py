0737e1aac79efaeb844fd69a7853bb619477ec61
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-05-09 15:08:30 -0300

    Update dowtube.py

--- dowtube.py
+++ dowtube.py
@@ -22,7 +22,7 @@ def inputlink():
     yt = YouTube(link, on_progress_callback = on_progress)
 
     print("Titulo = ", yt.title)
-    print("Baixando...")
+    print("Baixando..")
 
     ys = yt.streams.get_highest_resolution()
 
